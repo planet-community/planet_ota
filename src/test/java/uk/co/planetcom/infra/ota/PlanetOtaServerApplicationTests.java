a9d892d4b941ef4fa6c3a312fe0d8d130d9de16a
Dom Rodriguez <shymega@shymega.org.uk>
2022-08-11 15:29:02 +0100

    feat(code): Initial commit
    Signed-off-by: Dom Rodriguez <shymega@shymega.org.uk>

---
+++ src/test/java/uk/co/planetcom/infra/ota/PlanetOtaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package uk.co.planetcom.infra.ota;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlanetOtaServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
