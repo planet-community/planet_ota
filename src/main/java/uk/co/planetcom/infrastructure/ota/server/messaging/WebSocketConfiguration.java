0cbe763865cb2ade91633cd1a396ac47154ee9fb
Dom Rodriguez <shymega@shymega.org.uk>
2022-12-13 21:11:24 +0000

    feat(STOMP): Add initial support for STOMP over WebSocketsSigned-off-by: Dom Rodriguez <shymega@shymega.org.uk>

---
+++ src/main/java/uk/co/planetcom/infrastructure/ota/server/messaging/WebSocketConfiguration.java
@@ -0,0 +1,23 @@
+package uk.co.planetcom.infrastructure.ota.server.messaging;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfiguration implements WebSocketMessageBrokerConfigurer {
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry config) {
+        config.enableSimpleBroker("/topic");
+        config.setApplicationDestinationPrefixes("/ota");
+    }
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/ota-app-websocket")
+                .withSockJS();
+    }
+}
