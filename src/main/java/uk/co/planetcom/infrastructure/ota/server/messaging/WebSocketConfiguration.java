e45e38756d554df38ccf5ef7df1e73cbccaa4546
Dom Rodriguez <shymega@shymega.org.uk>
2022-12-16 15:35:47 +0000

    feat(logger): Add Slf4j annotation to WebSocketConfiguration.classSigned-off-by: Dom Rodriguez <shymega@shymega.org.uk>

--- src/main/java/uk/co/planetcom/infrastructure/ota/server/messaging/WebSocketConfiguration.java
+++ src/main/java/uk/co/planetcom/infrastructure/ota/server/messaging/WebSocketConfiguration.java
@@ -6,6 +6,7 @@ import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBr
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
+@Slf4j
 @Configuration
 @EnableWebSocketMessageBroker
 public class WebSocketConfiguration implements WebSocketMessageBrokerConfigurer {
