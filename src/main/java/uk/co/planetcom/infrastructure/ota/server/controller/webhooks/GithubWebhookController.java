dfece067a07078099cb0b274eaea874581f86fbf
Dom Rodriguez <shymega@shymega.org.uk>
2022-12-16 15:33:56 +0000

    refactor(codebase): Introduce logging, run a linter, and change error codesSigned-off-by: Dom Rodriguez <shymega@shymega.org.uk>

--- src/main/java/uk/co/planetcom/infrastructure/ota/server/controller/webhooks/GithubWebhookController.java
+++ src/main/java/uk/co/planetcom/infrastructure/ota/server/controller/webhooks/GithubWebhookController.java
@@ -8,8 +8,11 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import lombok.extern.slf4j.Slf4j;
+
 import java.security.MessageDigest;
 
+@Slf4j
 @RestController
 @RequestMapping("/webhooks/github")
 public class GithubWebhookController {
@@ -18,14 +21,21 @@ public class GithubWebhookController {
     @Value("${github.webhooks.secrets.codid}")
     private String GITHUB_CODID_WEBHOOK_SECRET;
 
-    @PostMapping(value = "/codid", produces = MediaType.APPLICATION_JSON_VALUE, consumes =
-            MediaType.APPLICATION_JSON_VALUE)
+    @PostMapping(value = "/codid", produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<String> codidWebhook(@RequestHeader("X-Hub-Signature") String sig,
-                                               @RequestBody String payload) {
-        if (null == sig || sig.isEmpty()) {
+            @RequestBody String payload) {
+        log.info("GitHub webhook: Received new webhook request.");
+        try {
+            if (null == sig || sig.isEmpty()) {
+                log.warn("No signature provided, return 400.");
+                return new ResponseEntity<>("No signature provided.\n", HttpStatus.BAD_REQUEST);
+            }
+        } catch (NullPointerException e) {
+            log.warn("Null pointer encountered, this could merely be an empty signature. Return 400.");
             return new ResponseEntity<>("No signature provided.\n", HttpStatus.BAD_REQUEST);
         }
         try {
+            log.debug("Computing GitHub SHA-1 hash..");
             String computedHash = String.format("sha1=%s", new HmacUtils(HmacAlgorithms.HMAC_SHA_1,
                     GITHUB_CODID_WEBHOOK_SECRET.getBytes()).hmacHex(payload.getBytes()));
 
@@ -33,8 +43,12 @@ public class GithubWebhookController {
                 return new ResponseEntity<>("Invalid signature.\n", HttpStatus.UNAUTHORIZED);
             }
         } catch (Exception e) {
-            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+            log.error("We encountered an exception. It's probably an error comparing the hash, but we don't want to tell the client. Return 500.");
+            // Bit of explanation - we don't want to make it obvious there was an exception, because malicious attackers could use this knowledge.
+            // Instead, we return BAD_REQUEST (400) status code.
+            return new ResponseEntity<>("Invalid signature.\n", HttpStatus.BAD_REQUEST);
         }
+        log.info("GitHub webhook: Signature processed all fine. ");
 
         return new ResponseEntity<>(HttpStatus.ACCEPTED);
     }
