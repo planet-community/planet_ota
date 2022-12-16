7aed5d3740244292a5357a19d61e560c49bd2c97
Dom Rodriguez <shymega@shymega.org.uk>
2022-12-16 15:35:29 +0000

    Return MalformedURLException message from util
    Signed-off-by: Dom Rodriguez <shymega@shymega.org.uk>

--- src/main/java/uk/co/planetcom/infrastructure/ota/server/util/UrlUtils.java
+++ src/main/java/uk/co/planetcom/infrastructure/ota/server/util/UrlUtils.java
@@ -1,17 +1,18 @@
-package uk.co.planetcom.infrastructure.ota.server.utils;
+package uk.co.planetcom.infrastructure.ota.server.util;
 
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.nio.file.Paths;
 
+@Slf4j
 public class UrlUtils {
     private UrlUtils() {}
-    
-    public static String getUrlFileName(String url) {
+
+    public static String getUrlFileName(String url) throws MalformedURLException {
         try {
             return Paths.get(new URL(url).getPath()).getFileName().toString();
         } catch (MalformedURLException e) {
-            throw new RuntimeException(e);
+            throw new MalformedURLException("Error getting filename from path.");
         }
     }
 }
