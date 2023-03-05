d4c67fe209feb74a4ab9db4d6217bdd23b8c59f3
Dom Rodriguez <shymega@shymega.org.uk>
2023-03-05 16:49:19 +0100

    Add initial DB entities for Asset record storage

---
+++ src/main/java/uk/co/planetcom/infrastructure/ota/server/db/entity/Asset.java
@@ -0,0 +1,27 @@
+package uk.co.planetcom.infrastructure.ota.server.db.entity;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import uk.co.planetcom.infrastructure.ota.manifests.schemas.AvailableAsset;
+import uk.co.planetcom.infrastructure.ota.server.db.entity.converter.AssetTypeConverter;
+
+import java.time.ZonedDateTime;
+import java.util.UUID;
+
+@Data @AllArgsConstructor @NoArgsConstructor @ToString
+@Entity @Table(name = "assets")
+public class Asset {
+    @Id
+    @GeneratedValue
+    private UUID assetId;
+    private ZonedDateTime releaseTimeStamp;
+    private ZonedDateTime uploadTimeStamp;
+    @Convert(converter = AssetTypeConverter.class)
+    private AvailableAsset.AssetType assetType;
+
+
+}
