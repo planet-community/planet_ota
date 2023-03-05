d30c57ba7193712223748b155e54d71eab3fd403
Dom Rodriguez <shymega@shymega.org.uk>
2023-03-05 16:49:19 +0100

    feat(DB): Add initial DB entities for Asset record storage

---
+++ src/main/java/uk/co/planetcom/infrastructure/ota/server/db/repository/AssetsRepository.java
@@ -0,0 +1,11 @@
+package uk.co.planetcom.infrastructure.ota.server.db.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import uk.co.planetcom.infrastructure.ota.server.db.entity.Asset;
+
+import java.util.UUID;
+
+public interface AssetsRepository extends JpaRepository<Asset, UUID> {
+
+
+}
