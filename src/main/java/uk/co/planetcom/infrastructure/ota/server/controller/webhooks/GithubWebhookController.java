6094843688c732a8645d87db28f74ed3aa6e8122
Dom Rodriguez <shymega@shymega.org.uk>
2023-04-01 23:28:53 +0100

    refactor(webhook): Refactor GithubWebhookController classSigned-off-by: Dom Rodriguez <shymega@shymega.org.uk>

--- src/main/java/uk/co/planetcom/infrastructure/ota/server/controller/webhooks/GithubWebhookController.java
+++ src/main/java/uk/co/planetcom/infrastructure/ota/server/controller/webhooks/GithubWebhookController.java
@@ -1,5 +1,6 @@
 package uk.co.planetcom.infrastructure.ota.server.controller.webhooks;
 
+import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.codec.digest.HmacAlgorithms;
 import org.apache.commons.codec.digest.HmacUtils;
 import org.springframework.beans.factory.annotation.Value;
@@ -8,9 +9,9 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import lombok.extern.slf4j.Slf4j;
-
 import java.security.MessageDigest;
+import java.util.Collections;
+import java.util.Map;
 
 @Slf4j
 @RestController
@@ -22,24 +23,32 @@ public class GithubWebhookController {
     private String GITHUB_CODID_WEBHOOK_SECRET;
 
     @PostMapping(value = "/codid", produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<String> codidWebhook(@RequestHeader("X-Hub-Signature") String sig,
-            @RequestBody String payload) {
+    public ResponseEntity<Map> codidWebhook(@RequestHeader("X-Hub-Signature") String sig,
+                                            @RequestBody String payload) {
         log.info("GitHub webhook: Received new webhook request.");
         try {
-            log.info("Computing GitHub SHA-1 hash..");
-            String computedHash = String.format("sha1=%s", new HmacUtils(HmacAlgorithms.HMAC_SHA_1,
-                    GITHUB_CODID_WEBHOOK_SECRET.getBytes()).hmacHex(payload.getBytes()));
+            log.debug("Computing GitHub SHA-1 hash..");
+            byte[] computedHash = String.format("sha1=%s", new HmacUtils(HmacAlgorithms.HMAC_SHA_1,
+                    GITHUB_CODID_WEBHOOK_SECRET.getBytes()).hmacHex(payload.getBytes()))
+                    .getBytes();
 
-            if (sig.length() != SIG_LEN || MessageDigest.isEqual(sig.getBytes(), computedHash.getBytes())) {
-                return new ResponseEntity<>("Invalid signature.\n", HttpStatus.UNAUTHORIZED);
+            log.debug("Testing GitHub webhook validity and integrity.");
+            if (sig.length() != SIG_LEN || MessageDigest.isEqual(sig.getBytes(), computedHash)) {
+                log.warn("Warning: Webhook invalid.");
+                log.warn("Ignoring webhook call, and returning with 401.");
+                log.debug("For debugging purposes, webhook payload and signature are here:");
+                log.debug("Payload: ", payload);
+                log.debug("Signature: ", sig);
+
+                return new ResponseEntity<>(Collections.singletonMap("errorMessage", "INVALID_SIG"),
+                        HttpStatus.UNAUTHORIZED);
             }
         } catch (Exception e) {
             log.error("We encountered an exception. It's probably an error comparing the hash, but we don't want to tell the client. Return 500.");
-            // Bit of explanation - we don't want to make it obvious there was an exception, because malicious attackers could use this knowledge.
-            // Instead, we return BAD_REQUEST (400) status code.
-            return new ResponseEntity<>("Invalid signature.\n", HttpStatus.BAD_REQUEST);
+            return new ResponseEntity<>(Collections.singletonMap("errorMessage", "INVALID_SIG"),
+                    HttpStatus.UNAUTHORIZED);
         }
-        log.info("GitHub webhook: Signature processed all fine. ");
+        log.info("GitHub webhook: is valid.");
 
         return new ResponseEntity<>(HttpStatus.ACCEPTED);
     }
