06d15b70298a0bb85bb124e60216b1413a5c0afd
Dom Rodriguez <shymega@shymega.org.uk>
2023-04-01 23:32:44 +0100

    chore(codid): Remove CODID secret ValueSigned-off-by: Dom Rodriguez <shymega@shymega.org.uk>

--- src/main/java/uk/co/planetcom/infrastructure/ota/server/controller/webhooks/GitLabWebhookController.java
+++ src/main/java/uk/co/planetcom/infrastructure/ota/server/controller/webhooks/GitLabWebhookController.java
@@ -1,13 +1,10 @@
 package uk.co.planetcom.infrastructure.ota.server.controller.webhooks;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
 
 @Slf4j
 @RestController
 @RequestMapping("/webhooks/gitlab")
 public class GitLabWebhookController {
-    @Value("${gitlab.webhooks.secrets.codid}")
-    private String GITLAB_CODID_WEBHOOK_SECRET;
 }
