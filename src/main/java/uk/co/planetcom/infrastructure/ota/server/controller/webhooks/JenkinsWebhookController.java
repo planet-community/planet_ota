fd1ccc9f489687ce23a01e470669ccc84b422ce6
Dom Rodriguez <shymega@shymega.org.uk>
2023-04-01 23:32:55 +0100

    feat(webhook): Add Jenkins webhook controllerSigned-off-by: Dom Rodriguez <shymega@shymega.org.uk>

---
+++ src/main/java/uk/co/planetcom/infrastructure/ota/server/controller/webhooks/JenkinsWebhookController.java
@@ -0,0 +1,10 @@
+package uk.co.planetcom.infrastructure.ota.server.controller.webhooks;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+
+@Slf4j
+@RestController
+@RequestMapping("/webhooks/jenkins")
+public class JenkinsWebhookController {
+}
