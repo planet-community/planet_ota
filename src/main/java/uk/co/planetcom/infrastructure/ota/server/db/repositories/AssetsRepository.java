ce9081e0939e21de24b8aa69b64ec508236a3c53
Dom Rodriguez <shymega@shymega.org.uk>
2023-04-20 19:07:58 +0100

    Add `findAllByAssetVendor` method to Assets repo interface
    Signed-off-by: Dom Rodriguez <shymega@shymega.org.uk>

--- src/main/java/uk/co/planetcom/infrastructure/ota/server/db/repositories/AssetsRepository.java
+++ src/main/java/uk/co/planetcom/infrastructure/ota/server/db/repositories/AssetsRepository.java
@@ -3,10 +3,12 @@ package uk.co.planetcom.infrastructure.ota.server.db.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import uk.co.planetcom.infrastructure.ota.server.db.entities.Asset;
 import uk.co.planetcom.infrastructure.ota.server.db.entities.enums.AssetType;
+import uk.co.planetcom.infrastructure.ota.server.db.entities.enums.AssetVendor;
 
 import java.util.List;
 import java.util.UUID;
 
 public interface AssetsRepository extends JpaRepository<Asset, UUID> {
     List<Asset> findAllByAssetType(AssetType assetType);
+    List<Asset> findAllByAssetVendor(AssetVendor assetVendor);
 }
