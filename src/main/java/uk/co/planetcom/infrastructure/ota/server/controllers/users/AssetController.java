6ecd1866df93d13e864fc7a2106dd6eb8dd4e698
Dom Rodriguez <shymega@shymega.org.uk>
2023-04-21 15:56:15 +0100

    AssetController - set `produces` value for all methods
    Signed-off-by: Dom Rodriguez <shymega@shymega.org.uk>

--- src/main/java/uk/co/planetcom/infrastructure/ota/server/controllers/users/AssetController.java
+++ src/main/java/uk/co/planetcom/infrastructure/ota/server/controllers/users/AssetController.java
@@ -1,11 +1,14 @@
 package uk.co.planetcom.infrastructure.ota.server.controllers.users;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import uk.co.planetcom.infrastructure.ota.server.domain.Asset;
 import uk.co.planetcom.infrastructure.ota.server.enums.AssetType;
 import uk.co.planetcom.infrastructure.ota.server.enums.AssetVendor;
-import uk.co.planetcom.infrastructure.ota.server.db.repositories.AssetsRepository;
 import uk.co.planetcom.infrastructure.ota.server.services.AssetService;
 
 import java.util.Collection;
@@ -17,25 +20,24 @@ public class AssetController {
     @Autowired
     private AssetService assetService;
 
-    @GetMapping("/by/uuid/{uuid}")
+    @GetMapping(value = "/by/uuid/{uuid}", produces = MediaType.APPLICATION_JSON_VALUE)
     public Asset findByUuid(@PathVariable UUID uuid) {
         return assetService.findByUuid(uuid)
                 .orElseThrow();
     }
 
-    @GetMapping("/by/type/{assetType}")
+    @GetMapping(value = "/by/type/{assetType}", produces = MediaType.APPLICATION_JSON_VALUE)
     public Collection<Asset> findAssetByAssetType(@PathVariable AssetType assetType) {
         return assetService.findAllByAssetType(assetType);
     }
 
-    @GetMapping("/")
+    @GetMapping(value = "/", produces = MediaType.APPLICATION_JSON_VALUE)
     public Collection<Asset> findAssets() {
         return assetService.findAll();
     }
 
-    @GetMapping("/by/vendor/{vendorType}")
+    @GetMapping(value = "/by/vendor/{vendorType}", produces = MediaType.APPLICATION_JSON_VALUE)
     public Collection<Asset> findAssetByVendor(@PathVariable AssetVendor vendorType) {
         return assetService.findAllByVendorType(vendorType);
     }
-
 }
