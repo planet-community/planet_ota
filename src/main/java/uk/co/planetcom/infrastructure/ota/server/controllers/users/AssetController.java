72e5d3e9e032c5b8e6178228641fb4fb74589535
Dom Rodriguez <shymega@shymega.org.uk>
2023-04-21 17:10:45 +0100

    Add optional RequestHeader - `X-Planet-DeviceIdHash`
    Signed-off-by: Dom Rodriguez <shymega@shymega.org.uk>

--- src/main/java/uk/co/planetcom/infrastructure/ota/server/controllers/users/AssetController.java
+++ src/main/java/uk/co/planetcom/infrastructure/ota/server/controllers/users/AssetController.java
@@ -2,10 +2,7 @@ package uk.co.planetcom.infrastructure.ota.server.controllers.users;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import uk.co.planetcom.infrastructure.ota.server.domain.Asset;
 import uk.co.planetcom.infrastructure.ota.server.enums.AssetType;
 import uk.co.planetcom.infrastructure.ota.server.enums.AssetVendor;
@@ -21,23 +18,23 @@ public class AssetController {
     private AssetService assetService;
 
     @GetMapping(value = "/by/uuid/{uuid}", produces = MediaType.APPLICATION_JSON_VALUE)
-    public Asset findByUuid(@PathVariable UUID uuid) {
+    public Asset findByUuid(@PathVariable UUID uuid, @RequestHeader(value = "X-Planet-DeviceIdHash", required = false) String deviceIdHash) {
         return assetService.findByUuid(uuid)
                 .orElseThrow();
     }
 
     @GetMapping(value = "/by/type/{assetType}", produces = MediaType.APPLICATION_JSON_VALUE)
-    public Collection<Asset> findAssetByAssetType(@PathVariable AssetType assetType) {
+    public Collection<Asset> findAssetByAssetType(@PathVariable AssetType assetType, @RequestHeader(value = "X-Planet-DeviceIdHash", required = false) String deviceIdHash) {
         return assetService.findAllByAssetType(assetType);
     }
 
     @GetMapping(value = "/", produces = MediaType.APPLICATION_JSON_VALUE)
-    public Collection<Asset> findAssets() {
+    public Collection<Asset> findAssets(@RequestHeader(value = "X-Planet-DeviceIdHash", required = false) String deviceIdHash) {
         return assetService.findAll();
     }
 
     @GetMapping(value = "/by/vendor/{vendorType}", produces = MediaType.APPLICATION_JSON_VALUE)
-    public Collection<Asset> findAssetByVendor(@PathVariable AssetVendor vendorType) {
+    public Collection<Asset> findAssetByVendor(@PathVariable AssetVendor vendorType, @RequestHeader(value = "X-Planet-DeviceIdHash", required = false) String deviceIdHash) {
         return assetService.findAllByVendorType(vendorType);
     }
 }
