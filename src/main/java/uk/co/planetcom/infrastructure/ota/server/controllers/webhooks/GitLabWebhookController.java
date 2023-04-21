8697ef26321fd6edaf7a486074f52c6db2f4d9f8
Dom Rodriguez <shymega@shymega.org.uk>
2023-04-21 15:56:59 +0100

    fix(webhooks): Hide webhook controllers from OpenAPI docSigned-off-by: Dom Rodriguez <shymega@shymega.org.uk>

--- src/main/java/uk/co/planetcom/infrastructure/ota/server/controllers/webhooks/GitLabWebhookController.java
+++ src/main/java/uk/co/planetcom/infrastructure/ota/server/controllers/webhooks/GitLabWebhookController.java
@@ -1,11 +1,13 @@
 package uk.co.planetcom.infrastructure.ota.server.controllers.webhooks;
 
+import io.swagger.v3.oas.annotations.Hidden;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
 @RestController
+@Hidden
 @RequestMapping("/webhooks/gitlab")
 public class GitLabWebhookController {
 }
