8336470142ec1b1e2688985760a7673c87b169d6
Dom Rodriguez <shymega@shymega.org.uk>
2023-04-23 03:01:49 +0100

    Add more vendors
    Signed-off-by: Dom Rodriguez <shymega@shymega.org.uk>

--- src/main/java/uk/co/planetcom/infrastructure/ota/server/enums/AssetVendor.java
+++ src/main/java/uk/co/planetcom/infrastructure/ota/server/enums/AssetVendor.java
@@ -3,4 +3,10 @@ package uk.co.planetcom.infrastructure.ota.server.enums;
 public enum AssetVendor {
     PLANET,
     CODI_OS,
+    PC_LINEAGEOS_PORTS,
+    PC_SAILFISHOS_PORTS,
+    PC_POSTMARKETOS_PORTS,
+    PC_DROIDIAN_PORTS,
+    PC_NIXOS_PORTS,
+    GEMIAN,
 }
