9c6db0258c93a2a0df8fd81cd04cfe60e7db2ade
Dom Rodriguez <shymega@shymega.org.uk>
2023-07-30 23:18:18 +0100

    Remove Slf4j annotation

--- src/main/java/uk/co/planetcom/infrastructure/ota/server/domain/Asset.java
+++ src/main/java/uk/co/planetcom/infrastructure/ota/server/domain/Asset.java
@@ -7,7 +7,6 @@ import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.*;
-import lombok.extern.slf4j.Slf4j;
 import org.hibernate.annotations.DynamicUpdate;
 import uk.co.planetcom.infrastructure.ota.server.db.converters.UpdateChannelConverter;
 import uk.co.planetcom.infrastructure.ota.server.enums.AssetType;
@@ -22,7 +21,6 @@ import java.time.ZoneId;
 import java.time.ZonedDateTime;
 import java.util.UUID;
 
-@Slf4j
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
