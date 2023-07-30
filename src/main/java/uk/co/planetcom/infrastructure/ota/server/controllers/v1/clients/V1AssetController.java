315996ef17c70ab6e0c468c2042b99ea03e697c8
Dom Rodriguez <shymega@shymega.org.uk>
2023-07-30 23:17:34 +0100

    Remove `create-fw-asset` test endpoint

--- src/main/java/uk/co/planetcom/infrastructure/ota/server/controllers/v1/clients/V1AssetController.java
+++ src/main/java/uk/co/planetcom/infrastructure/ota/server/controllers/v1/clients/V1AssetController.java
@@ -3,17 +3,12 @@ package uk.co.planetcom.infrastructure.ota.server.controllers.v1.clients;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import uk.co.planetcom.infrastructure.ota.server.domain.Asset;
-import uk.co.planetcom.infrastructure.ota.server.domain.AssetCompat;
 import uk.co.planetcom.infrastructure.ota.server.enums.AssetType;
 import uk.co.planetcom.infrastructure.ota.server.enums.AssetVendor;
 import uk.co.planetcom.infrastructure.ota.server.enums.UpdateChannel;
 import uk.co.planetcom.infrastructure.ota.server.services.AssetService;
 
-import java.net.MalformedURLException;
-import java.net.URI;
-import java.time.ZonedDateTime;
 import java.util.Collection;
-import java.util.List;
 import java.util.UUID;
 
 @RequestMapping("/api/v1/assets")
@@ -48,30 +43,5 @@ public class V1AssetController extends V1ControllerBase {
         return assetService.findAllByUpdateChannel(channel);
     }
 
-    @PostMapping(value = "/create-fw-asset", produces = V1_API_ACCEPT_HEADER_VALUE)
-    public Asset createFwAsset() throws MalformedURLException {
-        AssetCompat newAssetCompat = AssetCompat.builder()
-                .coDiFwCompat(null)
-                .androidFwCompat(AssetCompat.AndroidFwCompat.builder()
-                        .baseBandCompatVersions(List.of("v1.0.0"))
-                        .build())
-                .build();
-        Asset newAsset = Asset.builder()
-                .assetCompat(newAssetCompat)
-                .assetFileName("astro-android-fw-v07-patch.zip")
-                .assetDownloadUri(URI.create("https://planet-aws-storage.amazonaws.com/astro-android-fw-v07-patch.zip"))
-                .assetSha256Hash("4t2jglkrhjfsgjfggwrg24467")
-                .assetType(AssetType.ASTRO_ANDROID_FW_PATCH)
-                .assetVendor(AssetVendor.PLANET)
-                .assetVersion("v07")
-                .updateChannel(UpdateChannel.STABLE)
-                .assetChangelog("Nothing much.")
-                .uploadTimeStamp(ZonedDateTime.now())
-                .releaseTimeStamp(ZonedDateTime.now().plusMinutes(6))
-                .build();
-
-        assetService.create(newAsset);
-
-        return newAsset;
     }
 }
