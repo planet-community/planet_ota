2f36653e6a15482d65a9b086e22b255dbcc8396b
Dom Rodriguez <shymega@shymega.org.uk>
2023-07-30 23:17:56 +0100

    fix(rest): Fix @RestController base class

--- src/main/java/uk/co/planetcom/infrastructure/ota/server/controllers/v1/clients/V1ControllerBase.java
+++ src/main/java/uk/co/planetcom/infrastructure/ota/server/controllers/v1/clients/V1ControllerBase.java
@@ -1,8 +1,5 @@
 package uk.co.planetcom.infrastructure.ota.server.controllers.v1.clients;
 
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
 public class V1ControllerBase {
   protected static final String V1_API_ACCEPT_HEADER_VALUE = "application/vnd.planet.agnostos-api.v1+json";
 }
