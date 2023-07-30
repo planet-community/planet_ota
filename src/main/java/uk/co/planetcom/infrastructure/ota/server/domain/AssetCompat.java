a3920fad007486e89627d268e24bcab0b5059bef
Dom Rodriguez <shymega@shymega.org.uk>
2023-07-30 23:29:17 +0100

    Run IDEA formatter on sources

--- src/main/java/uk/co/planetcom/infrastructure/ota/server/domain/AssetCompat.java
+++ src/main/java/uk/co/planetcom/infrastructure/ota/server/domain/AssetCompat.java
@@ -3,9 +3,6 @@ package uk.co.planetcom.infrastructure.ota.server.domain;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import jakarta.persistence.Embeddable;
 import lombok.*;
-import org.hibernate.annotations.JdbcTypeCode;
-import org.hibernate.type.SqlTypes;
-import org.springframework.data.relational.core.mapping.Embedded;
 
 @Data
 @AllArgsConstructor
