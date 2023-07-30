9abe30b564563d73bfd01fc7f3b350be2faf6c5f
Dom Rodriguez <shymega@shymega.org.uk>
2023-07-30 22:16:21 +0100

    refactor(DB): Move `AssetRepository` class to a different package

--- src/main/java/uk/co/planetcom/infrastructure/ota/server/db/AssetsRepository.java
+++ src/main/java/uk/co/planetcom/infrastructure/ota/server/db/AssetsRepository.java
@@ -1,9 +1,10 @@
-package uk.co.planetcom.infrastructure.ota.server.db.repositories;
+package uk.co.planetcom.infrastructure.ota.server.db;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import uk.co.planetcom.infrastructure.ota.server.domain.Asset;
 import uk.co.planetcom.infrastructure.ota.server.enums.AssetType;
 import uk.co.planetcom.infrastructure.ota.server.enums.AssetVendor;
+import uk.co.planetcom.infrastructure.ota.server.enums.UpdateChannel;
 
 import java.util.List;
 import java.util.UUID;
@@ -12,4 +13,6 @@ public interface AssetsRepository extends JpaRepository<Asset, UUID> {
     List<Asset> findAllByAssetType(AssetType assetType);
 
     List<Asset> findAllByAssetVendor(AssetVendor assetVendor);
+
+    List<Asset> findAllByUpdateChannel(UpdateChannel channel);
 }
