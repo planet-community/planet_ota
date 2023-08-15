958bfeab47ab70ce2a278da39ad542f378e7c887
Dom Rodriguez <shymega@shymega.org.uk>
2023-08-15 22:54:37 +0100

    feat(nix): Add Nix Flake support

---
+++ flake.nix
@@ -0,0 +1,32 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05";
+
+    flake-utils.url = "github:numtide/flake-utils";
+
+  };
+  outputs = { self, nixpkgs, flake-utils, ...}:
+
+  flake-utils.lib.eachDefaultSystem (system:
+  let
+    pkgs = import nixpkgs { inherit system; };
+    jar = pkgs.callPackage ./.jar.nix { };
+    jdtls = pkgs.callPackage ./.jdtls.nix { };
+  in
+  rec {
+    devShell = with pkgs; mkShellNoCC {
+      name = "java";
+      buildInputs = [
+        jdk17 jdtls # JDTLS requires java > 1.11
+
+        packages.jar
+      ];
+    };
+
+    packages = {
+      inherit jar;
+    };
+
+    defaultPackage = packages.jar;
+  });
+}
