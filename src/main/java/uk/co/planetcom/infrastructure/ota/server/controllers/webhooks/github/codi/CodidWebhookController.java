535d1cfbc5b79f95466c7f5a201085b1426736a0
Dom Rodriguez <shymega@shymega.org.uk>
2023-09-21 22:01:54 +0100

    Restructure controllers, use abstract classes

---
+++ src/main/java/uk/co/planetcom/infrastructure/ota/server/controllers/webhooks/github/codi/CodidWebhookController.java
@@ -0,0 +1,20 @@
+package uk.co.planetcom.infrastructure.ota.server.controllers.webhooks.github.codi;
+
+import io.swagger.v3.oas.annotations.Hidden;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import uk.co.planetcom.infrastructure.ota.server.controllers.webhooks.github.GitHubWebhookController;
+
+@Slf4j
+@RestController
+@Hidden
+@RequestMapping("/api/v1/webhooks/github")
+public class CodidWebhookController extends GitHubWebhookController {
+
+    @Value("${github.webhooks.secrets.codid}")
+    private String WEBHOOK_SECRET;
+    @Override
+    protected void dispatch() {}
+}
--- src/main/java/uk/co/planetcom/infrastructure/ota/server/controllers/webhooks/gitlab/GitLabWebhookController.java
+++ src/main/java/uk/co/planetcom/infrastructure/ota/server/controllers/webhooks/gitlab/GitLabWebhookController.java
@@ -1,4 +1,4 @@
-package uk.co.planetcom.infrastructure.ota.server.controllers.webhooks;
+package uk.co.planetcom.infrastructure.ota.server.controllers.webhooks.gitlab;
 
 import io.swagger.v3.oas.annotations.Hidden;
 import lombok.extern.slf4j.Slf4j;
@@ -6,8 +6,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
-@RestController
-@Hidden
-@RequestMapping("/webhooks/gitlab")
-public class GitLabWebhookController {
+public abstract class GitLabWebhookController {
+    abstract void dispatch();
 }
