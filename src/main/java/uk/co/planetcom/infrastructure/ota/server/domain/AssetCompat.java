f8bad192897160dd97ce66d2d84065e4bc0bd0fa
Dom Rodriguez <shymega@shymega.org.uk>
2023-09-21 22:05:33 +0100

    Refactor `AssetCompat.class`, etc, into one nested class

--- src/main/java/uk/co/planetcom/infrastructure/ota/server/domain/AssetCompat.java
+++ src/main/java/uk/co/planetcom/infrastructure/ota/server/domain/AssetCompat.java
@@ -2,7 +2,11 @@ package uk.co.planetcom.infrastructure.ota.server.domain;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import jakarta.persistence.Embeddable;
+import jakarta.persistence.Embedded;
 import lombok.*;
+import org.aspectj.weaver.ast.And;
+
+import java.util.List;
 
 @Data
 @AllArgsConstructor
@@ -12,6 +16,29 @@ import lombok.*;
 @Embeddable
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class AssetCompat {
-    private AssetCompatCoDiFw compatCoDiFw;
-    private AssetCompatAndroidFw compatAndroidFw;
+    @Embedded
+    private CoDiFw compatCoDiFw;
+    @Embedded
+    private AndroidFw compatAndroidFw;
+
+    @Data
+    @Builder
+    @Embeddable
+    @AllArgsConstructor
+    @NoArgsConstructor
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    public static class CoDiFw {
+        private List<String> resourcesCompatVersions;
+        private List<String> ospiCompatVersions;
+    }
+
+    @Data
+    @Builder
+    @Embeddable
+    @AllArgsConstructor
+    @NoArgsConstructor
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    public static class AndroidFw {
+        private List<String> baseBandCompatVersions;
+    }
 }
