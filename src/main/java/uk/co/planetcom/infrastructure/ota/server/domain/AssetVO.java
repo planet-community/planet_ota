6d8f49336dd8db5bd97a5e8825244cdc0d2cd0ea
Dom Rodriguez <shymega@shymega.org.uk>
2023-11-20 16:27:22 +0000

    refactor(Asset): Simplify class, and implement user-visible VO

--- src/main/java/uk/co/planetcom/infrastructure/ota/server/domain/AssetVO.java
+++ src/main/java/uk/co/planetcom/infrastructure/ota/server/domain/AssetVO.java
@@ -5,12 +5,7 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import io.swagger.v3.oas.annotations.Hidden;
 import jakarta.persistence.*;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
-import lombok.*;
-import uk.co.planetcom.infrastructure.ota.server.db.converters.AssetTypeConverter;
-import uk.co.planetcom.infrastructure.ota.server.db.converters.AssetVendorConverter;
-import uk.co.planetcom.infrastructure.ota.server.db.converters.UpdateChannelConverter;
+import lombok.Data;
 import uk.co.planetcom.infrastructure.ota.server.enums.AssetType;
 import uk.co.planetcom.infrastructure.ota.server.enums.AssetVendor;
 import uk.co.planetcom.infrastructure.ota.server.enums.UpdateChannel;
@@ -23,77 +18,55 @@ import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
-@ToString
-@Builder
-@Entity(name = "Asset")
 @JsonInclude(JsonInclude.Include.NON_NULL)
-@Table(name = "assets")
-// TODO: Javadoc and OpenAPI doc this class.
-public class Asset implements Serializable {
-    @Id
-    @GeneratedValue(strategy = GenerationType.UUID)
-    @NotNull
-    @Column(nullable = false, name = "id")
-    @Setter(AccessLevel.NONE)
+@Data
+public class AssetVO implements Serializable {
+    public AssetVO(Asset o) {
+        this.setAssetId(o.getAssetId());
+        this.setAssetFileName(o.getAssetFileName());
+        this.setAssetVendor(o.getAssetVendor());
+        this.setAssetVersion(o.getAssetVersion());
+        this.setAssetDownloadUri(o.getAssetDownloadUri());
+        this.setAssetChangelog(o.getAssetChangelog());
+        this.setAssetSha256Hash(o.getAssetSha256Hash());
+        this.setReleaseTimeStamp(o.getReleaseTimeStamp());
+        this.setAssetType(o.getAssetType());
+        this.setUpdateChannel(o.getUpdateChannel());
+        this.setAssetCompat(o.getAssetCompat());
+        this.setAssetSuppressed(o.isAssetSuppressed());
+        this.setUploadTimeStamp(o.getUploadTimeStamp());
+    }
+
     private UUID assetId; /* UUID/GUID to avoid column collision */
 
-    @NotNull
-    @NotBlank
-    @Column(nullable = false)
     private String assetFileName; /* String representation of the filename that the object was uploaded as. */
 
-    @NotNull
-    @Column(nullable = false)
-    @Convert(converter = AssetVendorConverter.class)
     private AssetVendor assetVendor; /* Vendor of Asset. */
 
-    @NotNull
-    @NotBlank
-    @Column(nullable = false)
     private String assetVersion; /* Arbitrary String denoting the version of the Asset */
 
-    @NotNull
-    @Column(nullable = false)
     private URI assetDownloadUri; /* A URI to the asset. */
 
-    @NotNull
-    @ElementCollection
-    @Column(nullable = false)
     private List<String> assetChangelog; /* Newline delimited String of changes in this asset. */
 
-    @NotNull
-    @NotBlank
-    @Column(nullable = false)
     private String assetSha256Hash; /* SHA-256 hash of the asset, generate from bytes stored in RDBMS */
 
-    @NotNull
-    @Column(nullable = false)
-    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY) /* Restrict access from public API. */
-    private ZonedDateTime releaseTimeStamp; /* When the asset is 'due' to be released to users. */
-
-    @Convert(converter = AssetTypeConverter.class)
-    @Column(nullable = false)
-    @NotNull
-    private AssetType assetType; /* Can be queried from the `AssetService` class. */
-
-    @Convert(converter = UpdateChannelConverter.class)
-    @Column(nullable = false)
-    @NotNull
-    private UpdateChannel updateChannel; /* Channel that the update is released on. */
-
-    @Column(nullable = false)
-    @Embedded
-    @NotNull
-    private AssetCompat assetCompat;
-
-    @Column(nullable = false)
     @JsonProperty(access = JsonProperty.Access.WRITE_ONLY) /* Restrict access from public API. */
     @Hidden
-    // By default, not suppressed.
+    private ZonedDateTime releaseTimeStamp; /* When the asset is 'due' to be released to users. */
+
+    private AssetType assetType; /* Can be queried from the `AssetService` class. */
+
+    private UpdateChannel updateChannel; /* Channel that the update is released on. */
+
+    private AssetCompat assetCompat;
+
+    @Hidden
+    @JsonIgnore
     private boolean assetSuppressed; /* Whenever the asset has been suppressed, for whatever reason. */
+
+    @JsonIgnore
+    private ZonedDateTime uploadTimeStamp;
 
     @Transient
     @JsonIgnore
@@ -101,19 +74,12 @@ public class Asset implements Serializable {
     private final ZoneId timeZone = ZoneId.of(Optional.ofNullable(System.getenv("TZ"))
         .orElse("Europe/London"));
 
-    @NotNull
-    @Column(nullable = false)
-    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY) /* Restrict access from public API. */
-    private ZonedDateTime uploadTimeStamp = ZonedDateTime.now(timeZone);
-
-    @Transient
     @JsonIgnore
     @Hidden
     public boolean isAvailable() {
         return !(this.releaseTimeStamp.isAfter(ZonedDateTime.now(this.timeZone)) && !this.isAssetSuppressed());
     }
 
-    @Transient
     @JsonIgnore
     @Hidden
     public boolean isNotAvailable() {
