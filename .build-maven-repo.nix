c59ae60655f8978285ebe944fde81ab5797c43d7
Dom Rodriguez <shymega@shymega.org.uk>
2023-12-23 17:45:50 +0000

    chore(Maven): Update Nix Maven outputHash

--- .build-maven-repo.nix
+++ .build-maven-repo.nix
@@ -33,5 +33,5 @@ stdenv.mkDerivation {
   outputHashAlgo = "sha256";
   outputHashMode = "recursive";
   # replace this with the correct SHA256
-  outputHash = "sha256-yEzjlyBRPCPmohTzPu4ifQQ7eX7spoW/3uw0DRdElPY=";
+  outputHash = "sha256-8xd2red2MEwiXT2P61kt3y6xDP26/jd3WsRmoij/BJA=";
 }
