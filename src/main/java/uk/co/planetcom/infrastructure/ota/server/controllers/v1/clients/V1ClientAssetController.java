e6f23ca9554883dcd78ad77303cf2927241dae36
Dom Rodriguez <shymega@shymega.org.uk>
2024-01-10 18:55:13 +0000

    refactor: Refactor AssetController to return Asset 'virtual object'
    For public consumption.
    
    Also:
    
    - Add initial docs for `findAssets()`.
    - Use `ResponseEntity<T>`.

--- src/main/java/uk/co/planetcom/infrastructure/ota/server/controllers/v1/clients/V1ClientAssetController.java
+++ src/main/java/uk/co/planetcom/infrastructure/ota/server/controllers/v1/clients/V1ClientAssetController.java
@@ -31,45 +31,56 @@ public final class V1ClientAssetController {
     private AssetService assetService;
 
     @GetMapping(value = "/", produces = V1_API_ACCEPT_HEADER_VALUE)
-    public Collection<Asset> findAssets() {
-        return assetService.findAll();
+    @Operation(summary = "Get all *available* assets", description = "Get a JSON array of all *available* Assets")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "Successful operation"),
+        @ApiResponse(responseCode = "500", description = "Errored operation")
+    })
+    public ResponseEntity<Collection<AssetVO>> findAssets() {
+        return ResponseEntity.ok(assetService.findAll()
+            .stream()
+            .map(AssetVO::new)
+            .collect(Collectors.toList())); // FIXME: Dangerous - handle errors with try/catch.
     }
 
     @GetMapping(value = "/by/uuid/{uuid}", produces = V1_API_ACCEPT_HEADER_VALUE)
-    public Asset findByUuid(@PathVariable UUID uuid) {
-        return assetService.findByUuid(uuid)
-            .orElseThrow();
+    public ResponseEntity<AssetVO> findByUuid(@PathVariable UUID uuid) {
+        return ResponseEntity.ok(assetService.findAll()
+            .stream()
+            .map(AssetVO::new)
+            .findFirst()
+            .orElseThrow()); // FIXME: Dangerous - handle errors with try/catch.
     }
 
     @GetMapping(value = "/by/availability/available", produces = V1_API_ACCEPT_HEADER_VALUE)
-    public Collection<AssetVO> findAllAssetsAvailable() {
-        return assetService.findAllAvailable()
+    public ResponseEntity<Collection<AssetVO>> findAllAssetsAvailable() {
+        return ResponseEntity.ok(assetService.findAllAvailable()
             .stream()
             .map(AssetVO::new)
-            .collect(Collectors.toList());
+            .collect(Collectors.toList())); // FIXME: Dangerous - handle errors with try/catch.
     }
 
     @GetMapping(value = "/by/availability/unavailable", produces = V1_API_ACCEPT_HEADER_VALUE)
-    public Collection<AssetVO> findAllAssetsNotAvailable() {
-        return assetService.findAllUnavailable()
+    public ResponseEntity<Collection<AssetVO>> findAllAssetsNotAvailable() {
+        return ResponseEntity.ok(assetService.findAllUnavailable()
             .stream()
             .map(AssetVO::new)
-            .collect(Collectors.toList());
+            .collect(Collectors.toList())); // FIXME: Dangerous - handle errors with try/catch.
     }
 
     @GetMapping(value = "/by/type/{assetType}", produces = V1_API_ACCEPT_HEADER_VALUE)
-    public Collection<AssetVO> findAssetByAssetType(@PathVariable AssetType assetType) {
-        return assetService.findAllByAssetType(assetType)
+    public ResponseEntity<Collection<AssetVO>> findAssetByAssetType(@PathVariable AssetType assetType) {
+        return ResponseEntity.ok(assetService.findAllByAssetType(assetType)
             .stream()
             .map(AssetVO::new)
-            .collect(Collectors.toList());
+            .collect(Collectors.toList())); // FIXME: Dangerous - handle errors with try/catch.
     }
 
     @GetMapping(value = "/by/vendor/{vendorType}", produces = V1_API_ACCEPT_HEADER_VALUE)
-    public Collection<AssetVO> findAssetByVendor(@PathVariable AssetVendor vendorType) {
-        return assetService.findAllByVendorType(vendorType)
+    public ResponseEntity<Collection<AssetVO>> findAssetByVendor(@PathVariable AssetVendor vendorType) {
+        return ResponseEntity.ok(assetService.findAllByVendorType(vendorType)
             .stream()
             .map(AssetVO::new)
-            .collect(Collectors.toList());
+            .collect(Collectors.toList())); // FIXME: Dangerous - handle errors with try/catch.
     }
 }
