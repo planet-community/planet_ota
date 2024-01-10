9f92ac4a22e71dfcf1892c8cf96f97c8ccdf0097
Dom Rodriguez <shymega@shymega.org.uk>
2024-01-10 18:42:39 +0000

    refactor: Rename GitLab webhook controller -> Planet Git

--- src/main/java/uk/co/planetcom/infrastructure/ota/server/controllers/webhooks/gitlab/GitLabWebhookController.java
+++
@@ -1,8 +0,0 @@
-package uk.co.planetcom.infrastructure.ota.server.controllers.webhooks.gitlab;
-
-import lombok.extern.slf4j.Slf4j;
-
-@Slf4j
-public abstract class GitLabWebhookController {
-    abstract void dispatch();
-}
