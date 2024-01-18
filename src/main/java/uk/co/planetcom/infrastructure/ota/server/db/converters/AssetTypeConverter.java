dbe99a544723d017e60cfa627bf7bd1290cadac7
Dom Rodriguez <shymega@shymega.org.uk>
2024-01-18 21:06:14 +0000

    refactor(converters): Adjust `IllegalArgumentException` error msg

--- src/main/java/uk/co/planetcom/infrastructure/ota/server/db/converters/AssetTypeConverter.java
+++ src/main/java/uk/co/planetcom/infrastructure/ota/server/db/converters/AssetTypeConverter.java
@@ -8,14 +8,14 @@ import uk.co.planetcom.infrastructure.ota.server.enums.AssetType;
 public class AssetTypeConverter implements AttributeConverter<AssetType, String> {
     @Override
     public String convertToDatabaseColumn(AssetType assetType) {
-        if (assetType == null) throw new IllegalArgumentException("DB error. Argument is null.");
+        if (assetType == null) throw new IllegalArgumentException("Cannot convert to DB column; `AssetType` argument is null.");
 
         return assetType.toString();
     }
 
     @Override
     public AssetType convertToEntityAttribute(String s) {
-        if (s == null || s.isEmpty()) throw new IllegalArgumentException("DB error. Argument is null.");
+        if (s == null || s.isEmpty()) throw new IllegalArgumentException("Cannot convert to Entity; String argument (`AssetType`) is null.");
 
         return AssetType.valueOf(s);
     }
--- src/main/java/uk/co/planetcom/infrastructure/ota/server/db/converters/AssetVendorConverter.java
+++ src/main/java/uk/co/planetcom/infrastructure/ota/server/db/converters/AssetVendorConverter.java
@@ -8,11 +8,15 @@ import uk.co.planetcom.infrastructure.ota.server.enums.AssetVendor;
 public class AssetVendorConverter implements AttributeConverter<AssetVendor, String> {
     @Override
     public String convertToDatabaseColumn(AssetVendor assetVendor) {
+        if (assetVendor == null) throw new IllegalArgumentException("Cannot convert to DB column; `AssetVendor` argument is null.");
+
         return assetVendor.toString();
     }
 
     @Override
     public AssetVendor convertToEntityAttribute(String s) {
+        if (s == null || s.isEmpty()) throw new IllegalArgumentException("Cannot convert to Entity; String argument (`AssetType`) is null.");
+
         return AssetVendor.valueOf(s);
     }
 }
