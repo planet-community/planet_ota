3fc0532e79eb4d2e542548c2871a1beaf6419675
Dom Rodriguez <shymega@shymega.org.uk>
2024-01-25 19:31:23 +0000

    feat(Product): Add Quirks enum
    For things like keyboards, cellular ranges
    
    Signed-off-by: Dom Rodriguez <shymega@shymega.org.uk>

---
+++ src/main/java/uk/co/planetcom/infrastructure/ota/server/enums/AssetProductQuirkEnum.java
@@ -0,0 +1,10 @@
+package uk.co.planetcom.infrastructure.ota.server.enums;
+
+public enum AssetProductQuirkEnum {
+    CELLULAR_VERIZON,
+    CELLULAR_JAPAN,
+    CELLULAR_WORLDWIDE,
+    KEYBOARD_QWERTY,
+    KEYBOARD_QWERTZ,
+    KEYBOARD_ALL,
+}
